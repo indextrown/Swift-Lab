7a353e9b98177a8a552797f7927aba049d17bbe2
indextrown <indextrown@gmail.com>
2026-02-19 23:13:23 +0900

    Initial Commit

---
+++ Swift-Lab/Swift_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_LabApp.swift
+//  Swift-Lab
+//
+//  Created by 김동현 on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
